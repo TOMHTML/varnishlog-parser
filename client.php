e970d3c42cd34c1b268f5bdc9e677cd2844efa79
TOMHTML <tomhtml@gmail.com>
2016-08-15 07:52:54 +0200

    Fix broken title for home page

--- client.php
+++ client.php
@@ -51,7 +51,11 @@ catch(\Exception $e){
 <html>
 <head>
   <meta charset="utf-8" />
-  <title>Varnishlog analysis for : <?php echo $FILENAME ?></title>
+  <?php if(empty($FILENAME) && empty($FILEPATH)): ?>
+    <title>Varnishlog analysis</title>
+  <?php else: ?>
+    <title>Varnishlog analysis for : <?php echo $FILENAME ?></title>
+  <?php endif; ?>
   <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
   <meta name="viewport" content="width=device-width, initial-scale=1">
