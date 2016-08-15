766a0b2aa1ddb48b243fc6c3dba52369cb0e46ac
TOMHTML <tomhtml@gmail.com>
2016-08-15 11:19:03 +0200

    Upgrade to Bootstrap CSS 3.3.7

--- client.php
+++ client.php
@@ -59,7 +59,7 @@ catch(\Exception $e){
     <title>Varnishlog analysis for : <?php echo $FILENAME ?></title>
   <?php endif; ?>
   <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
